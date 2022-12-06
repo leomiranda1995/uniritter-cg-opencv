3bd6e0d2250b13c68b3752f10771c6cb9cb84b8c
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-12-06 14:09:10 -0300

    Ajuste

--- Config.bat
+++ Config.bat
@@ -1,11 +1,11 @@
 pip install virtualenv
+pip install opencv-python
 virtualenv opencv
 cd opencv\Scripts
 activate
 cd..
 cd..
 cd src
-pip install opencv-python
 pip install numpy
 pip install matplotlib
 pause
