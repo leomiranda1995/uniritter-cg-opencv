6b13b6df21b9955f6e6bcbabd42dbf34dff4320d
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-12-06 12:19:09 -0300

    Configura ambiente

--- 1 - Config.bat
+++
@@ -1,5 +0,0 @@
-pip install opencv-python
-virtualenv opencv
-cd .\opencv\Scripts
-activate
-pause
---
+++ Config.bat
@@ -0,0 +1,11 @@
+pip install virutalenv
+pause
+virtualenv opencv
+cd opencv\Scripts
+activate
+pause
+cd..
+cd..
+cd src
+pip install matplotlib
+pause
