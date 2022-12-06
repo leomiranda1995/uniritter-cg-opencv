f865b52727ab3bea0004579bc8ed621b6b6b183c
Filipe <filpedebarros@hotmail.com>
2022-12-06 01:01:23 -0300

    Necessário pip install tk

--- src/metodos/images/imagemMetodos.py
+++ src/metodos/images/imagemMetodos.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
 import os
-from tkinter import filedialog
+from tkinter import filedialog #Necessário pip install tk
 
 def selecionaImagem():
-  # DESENVOLVER O INPUT DE IMAGEM PARA QUE O USUÁRIO SELECIONE
+  ## DESENVOLVER O INPUT DE IMAGEM PARA QUE O USUÁRIO SELECIONE
   selecao_imagem = filedialog.askopenfilename(filetypes=[("Arquivos PNG", "*.png"), ("Arquivos JPG", "*.jpg")])
   imagem = cv2.imread(selecao_imagem)
   return imagem
+
+
 
 def showImagem(imagem):
   from matplotlib import pyplot as plt
